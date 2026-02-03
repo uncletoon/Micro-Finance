68ca74eefe2898d81b44cbadb43360e083e1a6c9
uncletoon <ingabirepatience3@gmail.com>
2026-02-03 11:11:54 +0200

    feat: add endppoint for update user and, create, get and update account

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -50,6 +50,53 @@ const listUsers = (req, res) => {
 };
 
 
+// Update a user
+const updateUser = (req, res) => {
+    const { id } = req.params;
+
+    if(!users[id]){
+        return res.status(400).json({
+            message: "User not found"
+        });
+    }
+
+    const ALLOWED_LEY = ["name", "email"];
+    const updates = {};
+
+    for (const key of ALLOWED_LEY){
+        if( req.body[key] !== undefined){
+
+            if(key === "name" && typeof req.body[key] !== "string"){
+                return res.status(400).json({
+                    message: "Invalid name."
+                });
+            }
+
+            if(key === "email" && !req.body[key].includes("@")){
+                return res.status(400).json({
+                    message: "Invalid email."
+                })
+            }
+
+            updates[key] = req.body[key];
+        }
+    }
+
+    users[id] = {
+        ...users[id],
+        ...updates,
+        updated_at: new Date().toLocaleTimeString()
+    };
+
+    res.status(200).json({
+        message: "User updated successfully!!",
+        user: users
+    });
+  
+
+}
+
+// Retrieve a user by ID
 const getUserById = (req, res) => {
     const { id } = req.params;
 
@@ -68,7 +115,8 @@ const getUserById = (req, res) => {
 module.exports = {
     createUser,
     listUsers,
-    getUserById
+    getUserById,
+    updateUser,
 };
 
 
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -5,12 +5,16 @@ const userRouter = express.Router();
 const {
     createUser,
     listUsers,
-    getUserById
+    getUserById,
+    getUserByQuery,
+    updateUser
 } = require("../controllers/user.controller")
 
 userRouter.post("/", createUser)
 userRouter.get("/", listUsers)
 userRouter.get('/:id', getUserById)
+userRouter.patch('/:id', updateUser)
+userRouter.get('/search', getUserByQuery)
 
 
 
